6697231d8a291901f6b69023dc5870457a5d2725
Ruslan <ruslan.tretyak.94@gmail.com>
2024-09-13 20:08:51 +0300

    corrected imports

--- src/main/java/com/application/repository/OrderRepository.java
+++ src/main/java/com/application/repository/OrderRepository.java
@@ -1,6 +1,6 @@
 package com.application.repository;
 
-import com.application.model.Order;
+import com.application.model.entity.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
