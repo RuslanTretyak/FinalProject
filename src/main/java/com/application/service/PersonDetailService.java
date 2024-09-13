37d8da7cb71819dc3de0977a2678ff6cc9f7e572
Ruslan <ruslan.tretyak.94@gmail.com>
2024-09-13 20:09:45 +0300

    add person details and detail service

---
+++ src/main/java/com/application/service/PersonDetailService.java
@@ -0,0 +1,31 @@
+package com.application.service;
+
+import com.application.model.entity.Person;
+import com.application.repository.PersonRepository;
+import com.application.security.PersonDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PersonDetailService implements UserDetailsService {
+    private PersonRepository personRepository;
+
+    @Autowired
+    public PersonDetailService(PersonRepository personRepository) {
+        this.personRepository = personRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Person> person = personRepository.findByLogin(username);
+        if(person.isEmpty()){
+            throw new UsernameNotFoundException("User not found");
+        }
+        return new PersonDetails(person.get());
+    }
+}
